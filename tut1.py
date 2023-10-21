e4b1e40b4289035aaf8fc71b2d27aa43826fff2f
Deepak <you@example.com>
2023-10-21 12:41:18 +0530

    tut 1 and 2 added

---
+++ tut1.py
@@ -0,0 +1,19 @@
+# when u use +, it will concat the string
+print("hey"+"muskan")
+
+# it will the print all the string separated by space
+print("hey","muskan")
+
+# hello" world, use \ for escape the characters
+print("hello\" world")
+
+x=5
+# type will give u the type of datatype
+print(type(x))
+
+# typecasting the int variable to string
+str(x)
+
+# u can multiple str with int
+print("muskan"*2)
+
---
+++ tut2.py
@@ -0,0 +1,43 @@
+name = "Muskan"
+
+# to get the length of string
+print(len(name))
+
+print(name[5])
+
+print(name[-1])
+
+print(name[len(name)-1])
+
+
+"""
+M -> 0 -6
+u -> 1 -5
+s -> 2 -4
+k -> 3 -3
+a -> 4 -2
+n -> 5 -1
+ 
+"""
+
+# use this slcing if u want to extract multiple characters
+# end position will be excluded
+
+# variable[startPos:endPos] 
+
+print(name[0:4])
+
+print(name[:4])
+
+print(name[0:])
+
+print(name[0:5154545])
+
+
+# variable[startPos:endPos:jumpCount] 
+str="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+print(str[12:15:3]) #it will jump 2 characters and give u every 3rd character
+
+print(name.replace("Mus", "Dee"))
+
+
